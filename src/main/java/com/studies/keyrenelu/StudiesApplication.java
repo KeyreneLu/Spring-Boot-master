a905bc6f9d00648bd04c50a7c5d081cbe789e390
wang-qiang <KeyreneLu@gmail.cm>
2018-05-14 18:18:22 +0800

    chapter 0.0.1

---
+++ src/main/java/com/studies/keyrenelu/StudiesApplication.java
@@ -0,0 +1,12 @@
+package com.studies.keyrenelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/studies/keyrenelu/WebConfiguration.java
@@ -0,0 +1,47 @@
+package com.studies.keyrenelu;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public RemoteIpFilter getRemoteIpFilter(){
+        return new RemoteIpFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new myFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setName("myFilter");
+        registrationBean.setOrder(1);
+        registrationBean.addInitParameter("ppm","周梦婷");
+        return registrationBean;
+    }
+
+    public class myFilter implements Filter {
+
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+
+        }
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        }
+
+        @Override
+        public void destroy() {
+
+        }
+    }
+}
